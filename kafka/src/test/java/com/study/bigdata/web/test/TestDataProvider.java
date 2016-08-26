ed755a4a7d45635252e4f680cfeb61a744134e5d
xiulankaixin <602813011@qq.com>
2016-08-26 17:47:30 +0800

    For Test Jenkins automation test

---
+++ kafka/src/test/java/com/study/bigdata/web/test/TestDataProvider.java
@@ -0,0 +1,32 @@
+package com.study.bigdata.web.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestDataProvider {
+	
+	@Test
+	public void testCreateOrder(){
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testGetRequester(){
+		
+		Assert.assertTrue(true);
+	}
+    
+	@Test
+	public void testGetReplyer(){
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testGetOrder(){
+		
+		Assert.assertTrue(false);
+	}
+}
---
+++ kafka/src/test/java/com/study/bigdata/web/test/TestDidiOrder.java
@@ -0,0 +1,36 @@
+package com.study.bigdata.web.test;
+
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class TestDidiOrder  { 
+	
+	@Test
+	public void testCreateOrder(){
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testGetRequester(){
+		
+		Assert.assertTrue(true);
+	}
+    
+	@Test
+	public void testGetReplyer(){
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testGetOrder(){
+		
+		Assert.assertTrue(true);
+	}
+	
+
+}  
