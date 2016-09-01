12f943e578a17edd0689ce1c4c337eaf00a10452
xiulankaixin <602813011@qq.com>
2016-09-01 17:18:33 +0800

    update test case

--- kafka/src/test/java/com/dw/math/MathUtilTest.java
+++ kafka/src/test/java/com/dw/math/MathUtilTest.java
@@ -4,18 +4,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MathUtilTest {
-	@Test
-	public void test_max_1_2_3() {
-		assertEquals(3, MathUtil.max(1, 2, 3));
-	}
-
-	@Test
-	public void test_max_10_20_30() {
-		assertEquals(30, MathUtil.max(10, 20, 30));
-	}
-
-	@Test
-	public void test_max_100_200_300() {
-		assertEquals(300, MathUtil.max(100, 200, 300));
-	}
+	 @Test
+	  public void test_max_1_2_3() {
+	      assertEquals(3, MathUtil.max(1, 2, 3));
+	  }
+	  @Test
+	  public void test_max_1_3_2() {
+	      assertEquals(3, MathUtil.max(1, 3, 2));
+	  }
+	  @Test
+	  public void test_max_3_2_1() {
+	      assertEquals(3, MathUtil.max(3, 2, 1));
+	  }
+	  @Test
+	  public void test_max_0_0_0(){
+	      assertEquals(0, MathUtil.max(0, 0, 0));
+	  }
+	  @Test
+	  public void test_max_0_1_0(){
+	      assertEquals(1, MathUtil.max(0, 1, 0));
+	  }
 }
