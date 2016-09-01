7206877e9882991dd070e4e04c2f7f9da531365c
xiulankaixin <602813011@qq.com>
2016-09-01 16:31:43 +0800

    add test case

--- kafka/src/test/java/com/dw/math/MathUtilTest.java
+++ kafka/src/test/java/com/dw/math/MathUtilTest.java
@@ -1,9 +1,21 @@
 package com.dw.math;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
-  public class MathUtilTest {
-    @Test
-    public void test_max_1_2_3() {
-          assertEquals(3, MathUtil.max(1, 2, 3));
-    }
+
+public class MathUtilTest {
+	@Test
+	public void test_max_1_2_3() {
+		assertEquals(3, MathUtil.max(1, 2, 3));
+	}
+
+	@Test
+	public void test_max_10_20_30() {
+		assertEquals(30, MathUtil.max(10, 20, 30));
+	}
+
+	@Test
+	public void test_max_100_200_300() {
+		assertEquals(300, MathUtil.max(100, 200, 300));
+	}
 }
